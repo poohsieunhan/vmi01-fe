5f1a4b42e0adc1449abc07263678c4617d341472
linhdd.ewh <linh.do3>
2025-12-10 08:41:20 +0700

    config vite api v2

--- src/services/exportApi.js
+++ src/services/exportApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL + "/api/v1"; // Sử dụng đường dẫn tương đối để proxy hoạt động
+const API_BASE_URL = import.meta.env.VITE_API_URL + "api/v1"; // Sử dụng đường dẫn tương đối để proxy hoạt động
 
 const api = axios.create({
   baseURL: API_BASE_URL,
